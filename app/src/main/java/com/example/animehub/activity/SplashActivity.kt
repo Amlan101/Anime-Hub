39d257e955f04107af4eb290a66d7f825f0e72cf
Amlan101 <amlan.sahooricky@gmail.com>
2023-07-24 01:06:43 +0530

    Added an animation to the Splash Screen

--- app/src/main/java/com/example/animehub/activity/SplashActivity.kt
+++ app/src/main/java/com/example/animehub/activity/SplashActivity.kt
@@ -1,18 +1,29 @@
 package com.example.animehub.activity
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.animehub.R
 
+
 class SplashActivity : AppCompatActivity() {
 
-    private val splashDelay: Long = 3000
+    private val splashDelay: Long = 4000
+    private lateinit var imgSplashIcon: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        imgSplashIcon = findViewById(R.id.img_splash_icon)
+
+        // Start the animation for the splash icon
+        val fadeAnimation = ObjectAnimator.ofFloat(imgSplashIcon, "alpha", 0f, 1f)
+        fadeAnimation.duration = 2000 // 1 second
+        fadeAnimation.start()
 
         Handler().postDelayed({
             // Start the main activity here
